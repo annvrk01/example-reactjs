907cd38edd18053293e13271b44b6c64ccae3f53
annvrk01 <annvrk01@gmail.com>
2023-09-08 00:14:36 +0700

    Solving homework problems Session 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 // Câu lệnh import để file liên kết với các function của file được export
 import MyComponent from './lesson/components/MyComponent'
+import Session02Component from './lesson/components/Session02Component'
 function App() {
   return (
     <div className="App">
        <MyComponent name="Nguyen Van D"></MyComponent>
+
+       <Session02Component></Session02Component>
     </div>
   );
 }
